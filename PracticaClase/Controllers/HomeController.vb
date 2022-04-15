12fa79ccbbe3aadce24b6a697ecf71035aafa644
hgaleano <hgaleano4@gmail.com>
2022-04-14 21:31:00 -0400

    Agregar archivos de proyecto.

---
+++ PracticaClase/Controllers/HomeController.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class HomeController
+        Inherits Controller
+
+        ' GET: Home
+        Function Index() As ActionResult
+            Dim db As ColegioEntities = New ColegioEntities
+
+
+            Dim objAlumno As New Alumno
+
+            'objAlumno.Nombre = "Carolina Montiel"
+            'objAlumno.CarreraID = 3
+            'objAlumno.NroDocumento = "85544"
+            'objAlumno.Email = "carol@empresa.com.py"
+            'objAlumno.Telefono = "0991837655"
+            'db.Alumno.Add(objAlumno)
+            'db.SaveChanges()
+
+            objAlumno = db.Alumno.Find(4)
+            'objAlumno.Nombre = "Pedro Gonzalez Romero"
+            'db.Entry(objAlumno).State = System.Data.Entity.EntityState.Modified
+            'db.SaveChanges()
+
+            db.Alumno.Remove(objAlumno)
+            db.SaveChanges()
+
+
+            Dim ListAlumnos As New List(Of Alumno)
+            ListAlumnos = db.Alumno.ToList
+
+
+            Return View(ListAlumnos)
+        End Function
+    End Class
+End Namespace
---
+++ PracticaClase/Views/Home/Index.vbhtml
@@ -0,0 +1,26 @@
+﻿@ModelType IEnumerable(Of PracticaClase.Alumno)
+
+@Code
+    Layout = Nothing
+End Code
+
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta name="viewport" content="width=device-width" />
+    <title>Lista de Alumnos</title>
+</head>
+<body>
+    <h2>Lista de Alumnos</h2>
+
+    <ul>
+        @For Each item In Model
+        @<li>
+            @item.Nombre
+        </li>
+        Next
+    </ul>
+   
+</body>
+</html>
